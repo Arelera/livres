86b9647f95a810c94daaaa5361e8f86568f4d0ae
Arel <arel.yurtsevenler@konzek.com>
2021-04-29 12:33:48 +0300

    Added book search endpoint.

--- backend/src/routes/book.js
+++ backend/src/routes/book.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize')
 const Book = require('../models/book')
 
 const router = require('express').Router()
@@ -5,6 +6,20 @@ const router = require('express').Router()
 router.get('/', async (req, res) => {
   const books = await Book.findAll({ limit: 10 })
   res.send(books)
+})
+
+router.get('/search', async (req, res) => {
+  const { query } = req.query
+
+  const response = await Book.findAll({
+    where: { title: { [Op.like]: `%${query}%` } },
+  })
+
+  if (!response.length) {
+    return res.status(404).send([])
+  }
+
+  res.send(response)
 })
 
 module.exports = router
